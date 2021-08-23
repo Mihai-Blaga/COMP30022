8161f195207a674fc6975589cd6152f187f3635c
Deepanshu Bhanot <dbhanot@student.unimelb.edu.au>
2021-08-23 22:32:05 +1000

    added a new layout for displaying events

---
+++ app/src/main/java/com/cloudsurfers/crm/DisplayEvents.java
@@ -0,0 +1,14 @@
+package com.cloudsurfers.crm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_events);
+    }
+}
