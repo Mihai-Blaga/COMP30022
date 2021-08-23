ebc05805829e84b8c5bb5fe58f0c31bd3b437a1a
Deepanshu Bhanot <dbhanot@student.unimelb.edu.au>
2021-08-24 01:07:38 +1000

    fetches and makes a toast out of the events in the now +- 7 days

--- app/src/main/java/com/cloudsurfers/crm/DisplayEvents.java
+++ app/src/main/java/com/cloudsurfers/crm/DisplayEvents.java
@@ -1,14 +1,120 @@
 package com.cloudsurfers.crm;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.icu.util.Calendar;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class DisplayEvents extends AppCompatActivity {
+import java.util.Date;
 
+public class DisplayEvents extends AppCompatActivity implements View.OnClickListener {
+    Button fetchButton;
+    Cursor calendarCursor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_events);
+
+        // add listener to fetch button
+        fetchButton = findViewById(R.id.fetchButton);
+        fetchButton.setOnClickListener(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void createEvent(){
+        System.out.println("Creating an event!");
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2012, 0, 19, 7, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2012, 0, 19, 8, 30);
+        ContentResolver cr = getContentResolver();
+        ContentValues values = new ContentValues();
+        String summary = "Value";
+        values.put(CalendarContract.Events.DTSTART, String.valueOf(beginTime));
+        values.put(CalendarContract.Events.DTEND, String.valueOf(endTime));
+        values.put(CalendarContract.Events.TITLE, summary);
+        values.put(CalendarContract.Events.DESCRIPTION, summary);
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, "America/Los_Angeles");
+        values.put(CalendarContract.Events.EVENT_LOCATION, "");
+        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fetchButton:
+                System.out.println("Pressed Button");
+                // fetch button on click logic
+                // first check if the permission to read Calendar has been granted
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED){
+                    System.out.println("Returning because permission not granted");
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, 42);
+                    return;
+                }
+
+                long now = new Date().getTime();
+                Uri.Builder builder = Uri.parse("content://com.android.calendar/instances/when").buildUpon();
+
+
+                ContentUris.appendId(builder, now - DateUtils.DAY_IN_MILLIS * 7);
+                ContentUris.appendId(builder, now + DateUtils.DAY_IN_MILLIS * 7);
+
+
+                calendarCursor = getContentResolver().query(
+                        builder.build(),
+                        null,
+                        null,
+                        null,
+                        null
+                );
+
+                System.out.println("Got events : "+String.valueOf(
+                        calendarCursor.getCount()
+                ));
+
+                while(calendarCursor.moveToNext()){
+                    if (calendarCursor!=null){
+                        int id_start = calendarCursor.getColumnIndex(CalendarContract.Events.DTSTART);
+                        int id_title = calendarCursor.getColumnIndex(CalendarContract.Events.TITLE);
+                        int id_desc = calendarCursor.getColumnIndex(CalendarContract.Events.DESCRIPTION);
+                        int id_location = calendarCursor.getColumnIndex(CalendarContract.Events.EVENT_LOCATION);
+
+                        String beginTime = calendarCursor.getString(id_start);
+                        Calendar newCalendar = Calendar.getInstance();
+                        newCalendar.setTimeInMillis(Long.parseLong(beginTime));
+                        int mYear = newCalendar.get(Calendar.YEAR);
+                        int mMonth = newCalendar.get(Calendar.MONTH);
+                        int mDay = newCalendar.get(Calendar.DAY_OF_MONTH);
+
+                        String title = calendarCursor.getString(id_title);
+                        String description = calendarCursor.getString(id_desc);
+                        String location = calendarCursor.getString(id_location);
+
+                        Toast.makeText(this, mDay+"/"+mMonth+"/"+mYear +","+title+","+description+","+location,Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(this,"Events are over!",Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                break;
+        }
     }
 }
