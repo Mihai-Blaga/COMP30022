71363701deded1bb471ddea3c3b0f33ac0411193
James-Program <75956447+James-Program@users.noreply.github.com>
2021-08-26 17:58:59 +1000

    Moved the post email to the ComposeEmail activity

--- app/src/main/java/com/cloudsurfers/crm/ComposeEmail.kt
+++ app/src/main/java/com/cloudsurfers/crm/ComposeEmail.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
@@ -16,38 +17,14 @@ class ComposeEmail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.email)
+        setContentView(R.layout.activity_email)
 
-        val sendEmailBtn: Button = findViewById(R.id.button)
+        val sendEmailBtn= findViewById<Button>(R.id.button2)
         sendEmailBtn.setOnClickListener {
-                sendEmail()
+            sendEmail()
         }
     }
 
-    private fun sendEmail() { // maybe pass in these values (to,cc,subject,text) in the function definition
-        Log.i("Send email", "")
-        val to:Array<String> = arrayOf("cloud.surf.dev@gmail.com") // get these from the app page
-        //val cc:Array<String> = arrayOf("apples")
-        val emailIntent = Intent(Intent.ACTION_SEND)
-
-        emailIntent.data = Uri.parse("mailto:")
-        emailIntent.type = "message/rfc822"
-        emailIntent.putExtra(Intent.EXTRA_EMAIL, to)
-        //emailIntent.putExtra(Intent.EXTRA_CC, cc)
-        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Test Subject 1") // <--- Need to change these to text fields in the page that are assigned to vars.
-        emailIntent.putExtra(Intent.EXTRA_TEXT, R.id.inputField) // ---^
-
-        try {
-            startActivity(Intent.createChooser(emailIntent, "Send mail using..."))
-            finish()
-            Log.i("Finished sending email.", "")
-        } catch (e: Exception) {
-            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-        }
-
-    }
-
-    /*
     private fun sendEmail() { // maybe pass in these values (to,cc,subject,text) in the function definition
 
         // Parsing the email's text fields
@@ -79,7 +56,6 @@ class ComposeEmail : AppCompatActivity() {
         }
 
     }
-     */
 
     fun buttonOnClick(v: View) {
         sendEmail()
