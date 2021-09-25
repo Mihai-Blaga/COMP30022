baab8e11bc84a4b3d6435689552f135a229d38a5
Phil Yang <phil.yang@student.unimelb.edu.au>
2021-09-26 01:16:05 +1000

    Added message to show user if query returns no contacts

--- app/src/main/java/com/cloudsurfers/crm/pages/search/SearchableActivity.kt
+++ app/src/main/java/com/cloudsurfers/crm/pages/search/SearchableActivity.kt
@@ -4,7 +4,9 @@ import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.SearchView
+import android.widget.TextView
 import androidx.core.view.children
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -39,7 +41,7 @@ class SearchableActivity : MainActivity() {
         val chipGroup: ChipGroup = findViewById(R.id.search_chip_group)
 
         for (tag: String in tags) {
-            val chip: Chip = layoutInflater.inflate(R.layout.search_chip, null, false) as Chip
+            val chip: Chip = layoutInflater.inflate(R.layout.search_chip, chipGroup, false) as Chip
             chip.text = tag
             chipGroup.addView(chip)
         }
@@ -62,11 +64,11 @@ class SearchableActivity : MainActivity() {
 
     // Extract the selected tags as an array list
     private fun getSelectedTags(): ArrayList<String> {
-        return findViewById<ChipGroup>(R.id.search_chip_group).children.filter {
+        return ArrayList(findViewById<ChipGroup>(R.id.search_chip_group).children.filter {
             (it as Chip).isChecked
         }.map {
             (it as Chip).text as String
-        }.toList() as ArrayList<String>
+        }.toList())
     }
 
     override fun startActivity(intent: Intent?) {
@@ -87,12 +89,24 @@ class SearchableActivity : MainActivity() {
             intent.getStringExtra(SearchManager.QUERY)?.also { query ->
                 // TODO: Perform The Search
                 val tags: ArrayList<String>? = intent.getStringArrayListExtra("tags")
-                val contacts: ArrayList<Contact> = basicContactSearch(query)
+                val queryContacts: ArrayList<Contact> = basicContactSearch(query)
+
+                val recyclerView = findViewById<RecyclerView>(R.id.search_recycler_view)
+                val emptyTextView = findViewById<TextView>(R.id.search_no_data_text_view)
 
                 // Add the contents into an adapter class and put into RecyclerView
-                findViewById<RecyclerView>(R.id.search_recycler_view).apply {
-                    adapter = SearchAdapter(contacts)
+                recyclerView.apply {
+                    adapter = SearchAdapter(queryContacts)
                     layoutManager = LinearLayoutManager(this@SearchableActivity)
+                }
+
+                // In case of no data hide the recycler view and provide a message to the user
+                if (queryContacts.isEmpty()) {
+                    recyclerView.visibility = View.GONE
+                    emptyTextView.visibility = View.VISIBLE
+                } else {
+                    recyclerView.visibility = View.VISIBLE
+                    emptyTextView.visibility = View.GONE
                 }
             }
         }
