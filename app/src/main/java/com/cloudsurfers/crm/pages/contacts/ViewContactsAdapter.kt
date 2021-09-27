d51e9597789ccdf85796c7c50dbb1cb5179794f1
James-Program <75956447+James-Program@users.noreply.github.com>
2021-09-27 19:37:57 +1000

    Change of packages from andriodx to andriod, since there are more useful methods in android

--- app/src/main/java/com/cloudsurfers/crm/pages/contacts/ViewContactsAdapter.kt
+++ app/src/main/java/com/cloudsurfers/crm/pages/contacts/ViewContactsAdapter.kt
@@ -1,6 +1,7 @@
 package com.cloudsurfers.crm.pages.contacts
 
 import android.graphics.Bitmap
+import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,8 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
 import androidx.core.graphics.createBitmap
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
@@ -18,6 +21,12 @@ import com.cloudsurfers.crm.R
 import com.cloudsurfers.crm.functions.Contact
 import java.util.*
 import kotlin.collections.ArrayList
+import android.text.StaticLayout
+
+import android.text.TextPaint
+
+
+
 
 
 class ViewContactsAdapter(private val contacts: ArrayList<Contact>) :
@@ -34,7 +43,7 @@ class ViewContactsAdapter(private val contacts: ArrayList<Contact>) :
 
         init {
             // Define click listener for the ViewHolder's View.
-            view.setOnClickListener() {
+            view.setOnClickListener {
                 val activity: AppCompatActivity = view.context as AppCompatActivity
                 val c: Contact = Contact.readContact(contact, activity)
 
@@ -75,25 +84,46 @@ class ViewContactsAdapter(private val contacts: ArrayList<Contact>) :
         viewHolder.contact = contacts[position]
 
         // For drawing the circles
-        val tempBitmap = ImageBitmap(50,50, ImageBitmapConfig.Argb8888)
-        val canvas = Canvas(tempBitmap)
-        val paint = Paint()
+//        val tempBitmap = ImageBitmap(50,50, ImageBitmapConfig.Argb8888)
+//        val canvas = Canvas(tempBitmap)
+//        val paint = Paint()
         val rand = Random()
 
-        paint.color = Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256))
-
-        canvas.drawCircle(Offset(25f,25f),25f, paint)
-
-        viewHolder.contactImageView.setImageBitmap(tempBitmap.asAndroidBitmap())
-
-        Log.i("debug",viewHolder.contactImageView.matrix.toString())
+        // Using a different library
+        val bitmap2 = Bitmap.createBitmap(50,50,Bitmap.Config.ARGB_8888)
+        val canvas2 = Canvas(bitmap2)
+        val paint2 = Paint()
 
         // Letter to appear in the centre of the circle
         val letter: String? = contacts[position].name?.substring(0,1)
 
-//        viewHolder.contactImageView.apply {
-//            setImageResource(R.color.matte_orange)
-//        }
+//        paint.color = Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)).toArgb()
+        paint2.color = Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256)).toArgb()
+
+//        canvas.drawCircle(Offset(25f,25f),25f, paint)
+        canvas2.drawCircle(25f,25f,25f, paint2)
+        if (letter != null) {
+            canvas2.drawText(letter,25f,25f,Paint())
+        }
+
+//        val textPaint = TextPaint()
+//        textPaint.isAntiAlias = true
+//        textPaint.textSize = 16f * R.style.Theme_MaterialComponents_Dialog_FixedSize
+//        textPaint.color = -0x1000000
+//
+//        val width = textPaint.measureText(letter).toInt()
+//        val staticLayout = StaticLayout(
+//            letter, textPaint,
+//            width, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0, false
+//        )
+//        staticLayout.draw(canvas2)
+
+//        viewHolder.contactImageView.setImageBitmap(tempBitmap.asAndroidBitmap())
+        viewHolder.contactImageView.setImageBitmap(bitmap2)
+
+        Log.i("debug",viewHolder.contactImageView.matrix.toString()+"   ,   "+letter)
+
+
 
     }
 
