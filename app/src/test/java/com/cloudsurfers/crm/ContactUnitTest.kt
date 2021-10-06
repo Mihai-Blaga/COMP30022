12124129009329c075018a182a22ce00a80916cc
Mihai Blaga <mihai.blaga@outlook.com>
2021-10-06 16:02:15 +1100

    successfully mocked content providers

--- app/src/test/java/com/cloudsurfers/crm/ContactUnitTest.kt
+++ app/src/test/java/com/cloudsurfers/crm/ContactUnitTest.kt
@@ -1,5 +1,7 @@
 package com.cloudsurfers.crm
 
+import android.app.Activity
+import android.content.ContentResolver
 import android.content.Context
 import android.database.Cursor
 import android.provider.ContactsContract
@@ -10,14 +12,25 @@ import com.cloudsurfers.crm.functions.Contact.Companion.readContacts
 import com.cloudsurfers.crm.pages.main.MainActivity
 import com.google.common.truth.Truth.assertThat
 import org.junit.Assert
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.robolectric.Robolectric.setupActivity
+import android.database.MatrixCursor
+import android.test.mock.MockContentProvider
+import com.cloudsurfers.crm.functions.Contact
+import com.cloudsurfers.crm.functions.Contact.Companion.readContact
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.runners.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class ContactUnitTest {
+    @Mock
+    private lateinit var a: Activity
+
+    @Mock
+    private lateinit var resolver: ContentResolver
 
     @Test
     fun verifyTestsWork(){
@@ -31,11 +44,64 @@ class ContactUnitTest {
 
 
     @Test
-    fun readContacts_testQuery(){
-        val resolver = MockContentResolver()
-        val c = resolver.query(ContactsContract.Data.CONTENT_URI, null, null, null, null)
-        assertFalse(c!!.moveToFirst())
-        assertTrue(c.getColumnIndex(ContactsContract.Data.RAW_CONTACT_ID) >=0)
-        c.close()
+    fun readContact_creatingContact(){
+        val lookupArray = arrayOf(
+            ContactsContract.Data.RAW_CONTACT_ID,    // Contract class constant for the _ID column name
+            ContactsContract.Data.MIMETYPE,
+            ContactsContract.Data.DATA1,
+            ContactsContract.CommonDataKinds.GroupMembership.GROUP_ROW_ID
+        )
+
+        val exampleCursor = MatrixCursor(lookupArray, 4)
+
+        exampleCursor.addRow(arrayOf("1", ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, "0411111111", 1))
+        exampleCursor.addRow(arrayOf("1", ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE, "name@email.com", 1))
+        exampleCursor.addRow(arrayOf("1", ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, "John Doe", 1))
+        exampleCursor.addRow(arrayOf("1", ContactsContract.CommonDataKinds.Note.CONTENT_ITEM_TYPE, "Note", 1))
+
+        val c = Contact("1")
+
+        `when`(a.contentResolver).thenReturn(resolver)
+
+        `when`(resolver.query(ContactsContract.Data.CONTENT_URI, lookupArray, null, emptyArray<String>(), null))
+            .thenReturn(exampleCursor)
+
+        val out = readContact(c, a)
+
+        assertNotEquals(out, null)
     }
+
+    /*
+    @Test
+    fun readPhone(){
+        //Step 1: Create data you want to return and put it into a matrix cursor
+        //In this case I am mocking getting phone numbers from Contacts Provider
+        val exampleData = arrayOf("(979) 267-8509")
+        val examleProjection = arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER)
+        val matrixCursor = MatrixCursor(examleProjection)
+        matrixCursor.addRow(exampleData)
+
+        //Step 2: Create a stub content provider and add the matrix cursor as the expected result of the query
+        val mockProvider = HashMapMockContentProvider()
+        mockProvider.addQueryResult(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            matrixCursor
+        )
+
+        //Step 3: Create a mock resolver and add the content provider.
+        val mockResolver = MockContentResolver()
+        mockResolver.addProvider(
+            ContactsContract.AUTHORITY /*Needs to be the same as the authority of the provider you are mocking */,
+            mockProvider
+        )
+
+        //Step 4: Add the mock resolver to the mock context
+        val mockContext = ContextWithMockContentResolver(super.getContext())
+        mockContext.setContentResolver(mockResolver)
+
+        //Example Test
+        val underTest = ExampleClassUnderTest()
+        val result: String = underTest.getPhoneNumbers(mockContext)
+        assertEquals("(979) 267-8509", result)
+    }*/
 }
