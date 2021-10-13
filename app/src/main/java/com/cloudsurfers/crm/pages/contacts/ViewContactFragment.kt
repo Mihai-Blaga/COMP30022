8e3a2042cb7c7562c0be32aaf65aa4c12a9a1c09
xlelx <khantthurein@student.unimelb.edu.au>
2021-10-13 21:00:27 +1100

    Edited ViewContactFragment

--- app/src/main/java/com/cloudsurfers/crm/pages/contacts/ViewContactFragment.kt
+++ app/src/main/java/com/cloudsurfers/crm/pages/contacts/ViewContactFragment.kt
@@ -1,6 +1,5 @@
 package com.cloudsurfers.crm.pages.contacts
 
-import android.app.Activity
 import android.content.Intent
 import android.icu.util.Calendar
 import android.os.Build
@@ -13,18 +12,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.cloudsurfers.crm.R
 import com.cloudsurfers.crm.functions.CalendarUtil
 import com.cloudsurfers.crm.databinding.FragmentViewContactBinding
 import com.cloudsurfers.crm.functions.ComposeEmail
 import androidx.annotation.RequiresApi
+import androidx.core.os.bundleOf
 import androidx.core.view.children
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.cloudsurfers.crm.functions.Meeting
-import com.cloudsurfers.crm.pages.meetings.ViewMeetingsAdapter
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
+import com.cloudsurfers.crm.functions.Contact
 import com.google.android.flexbox.FlexboxLayout
 import com.google.android.material.chip.Chip
-import java.time.LocalDateTime
+
+
+
 
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,12 +36,14 @@ private const val ARG_PARAM2 = "email"
 private const val ARG_PARAM3 = "mobile"
 private const val ARG_PARAM4 = "notes"
 private const val ARG_PARAM5 = "tags"
+private const val ARG_PARAM6 = "contactID"
 
 class ViewContactFragment : Fragment() {
     private var name: String? = null
     private var email: String? = null
     private var mobile: String? = null
     private var notes: String? = null
+    private var contactID: String? = null
     private var tags: ArrayList<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,10 +54,12 @@ class ViewContactFragment : Fragment() {
             mobile = it.getString(ARG_PARAM3)
             notes = it.getString(ARG_PARAM4)
             tags = it.getStringArrayList(ARG_PARAM5)
+            contactID = it.getString(ARG_PARAM6)
+
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -65,29 +72,6 @@ class ViewContactFragment : Fragment() {
         sendEmailButton.setOnClickListener {
             val intent: Intent = ComposeEmail.getSendEmailIntent(email!!)
             startActivity(Intent.createChooser(intent, "Send mail using..."))
-        }
-
-        // Create meeting button intent
-        val createMeetingButton = binding.viewContactCreateMeetingButton
-        createMeetingButton.setOnClickListener {
-            val intent: Intent = CalendarUtil.getInsertEventIntent(
-                title = "",
-                contactEmail = email!!,
-                location = "",
-                dateTime = Calendar.getInstance(),
-                desc = ""
-            )
-            startActivity(intent)
-        }
-
-        // Populate RecyclerView with upcoming meetings
-
-        val meetings = getMeetingsForContact(activity as Activity)
-        if (meetings.isEmpty())
-            binding.viewContactUpcomingMeetingsLabel.text = resources.getString(R.string.view_contact_upcoming_meeting_label_no_meetings)
-        binding.viewContactUpcomingMeetingsRecyclerView.apply {
-            adapter = ViewMeetingsAdapter(meetings, true)
-            layoutManager = LinearLayoutManager(activity)
         }
 
         // Add tags to the chip group
@@ -111,23 +95,26 @@ class ViewContactFragment : Fragment() {
                             setText("")
                         }
                     },
-                    1000)
+                        1000)
                 }
 
                 override fun afterTextChanged(p0: Editable?) {}
             })
         }
 
+        binding.viewContactDeleteContactButton.setOnClickListener {
+            val success = Contact.deleteContact(requireActivity(), contactID!!)
+            if (success){
+                setFragmentResult("requestKey", bundleOf("refreshContacts" to true))
+                findNavController().popBackStack()
+            }
+            else{
+                Toast.makeText(activity,"Failed to delete contact, please try again.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
 
         return binding.root
-    }
-
-    // Retrieve the upcoming meetings for the contact
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun getMeetingsForContact(activity: Activity): ArrayList<Meeting> {
-        return Meeting.fetchAllMeetings(activity)?.filter {
-            LocalDateTime.now().isBefore(if (it.beginDate != null) it.beginDate else LocalDateTime.now()) && it.contactEmail.equals(email)
-        } as ArrayList<Meeting>
     }
 
     // Retrieve an ArrayList<String> of selected tags
